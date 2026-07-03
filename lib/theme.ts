73a5ce6f5e80a8a5aab53f1981f1c3c27485ceb7
Olga <oyakubovska80@gmail.com>
2026-07-03 16:17:12 +0200

    feat: add Material-UI theme configuration

---
+++ lib/theme.ts
@@ -0,0 +1,13 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: '#9C27B0',
+      light: '#e1bee7',
+    },
+  },
+});
+
+export default theme;
